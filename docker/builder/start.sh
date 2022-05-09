21f92ec7a7d352043b4ce129579ace8a71ebcfc9
Felipe Carlos <efelipecarlos@gmail.com>
2022-05-09 17:11:25 -0300

    build: introducing auxiliary tools to make the build easier (Closes #79)

---
+++ docker/builder/start.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# This file is part of GEO Knowledge Hub.
+# Copyright 2020-2021 GEO Secretariat.
+#
+# GEO Knowledge Hub is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+# 
+# General definitions
+#
+BASE_DIRECTORY=$PWD
+
+VERDACCIO_LOCAL_REGISTRY=http://127.0.0.1:4873
+VERDACCIO_DOCKER_REGISTRY=http://127.0.0.1:4873
+
+GEO_KNOWLEDGE_HUB_ORGANIZATION_URL=https://github.com/geo-knowledge-hub
+
+#
+# Auxiliary functions
+#
+
+# Name: extract_from_quotes
+# Description: Extract values between quotes in a string
+extract_from_quotes() {
+	echo "$1" | cut -d'"' -f 2
+}
+
+#
+# 1. Configuring the required services to build
+#
+docker-compose -f docker/builder/docker-compose.yml up -d
+
+#
+# 2. Extracting `geo-knowledge-hub` package version
+#
+
+# getting the package definition on `Pipefile`
+PIPFILE_DEPENDENCY_DEFINITION=`cat Pipfile | grep geo-knowledge-hub`
+
+# extracting the version with perl-based code from oHo's answer: 
+#   https://stackoverflow.com/questions/16817646/extract-version-number-from-a-string
+PIPFILE_DEPENDENCY_VERSION=`echo ${PIPFILE_DEPENDENCY_DEFINITION} | cut -d',' -f 3 | cut -d'"' -f 2`
+
+#
+# 3. Publishing JavaScript dependencies from `geo-knowledge-hub` to verdaccio
+#
+
+# cloning and installing the package
+git clone --branch ${PIPFILE_DEPENDENCY_VERSION} ${GEO_KNOWLEDGE_HUB_ORGANIZATION_URL}/geo-knowledge-hub
+
+# extract the dependencies from the `geo-knowledge-hub`
+# note: `@geo` is the scope defined to the `geo-knowledge-hub` packages
+JAVASCRIPT_DEPENDENCIES_VERSION=`cat geo-knowledge-hub/geo_knowledge_hub/modules/theme/webpack.py | grep -e @geo-knowledge-hub`
+JAVASCRIPT_DEPENDENCIES_ARRAY=(${JAVASCRIPT_DEPENDENCIES_VERSION//,/ })
+
+# publishing the packages!
+for ((i=0;i< ${#JAVASCRIPT_DEPENDENCIES_ARRAY[@]} ; i+=2));
+do
+		PACKAGE_NAME=`extract_from_quotes ${JAVASCRIPT_DEPENDENCIES_ARRAY[i]}`
+        PACKAGE_NAME=${PACKAGE_NAME//@geo-knowledge-hub\/''}
+
+		PACKAGE_VERSION=`extract_from_quotes ${JAVASCRIPT_DEPENDENCIES_ARRAY[i + 1]}`
+
+        # checking if is a branch or tag	
+        if [[ "$PACKAGE_VERSION" != b* ]]; then
+            PACKAGE_VERSION=v$PACKAGE_VERSION
+        fi 
+        
+        git clone --branch $PACKAGE_VERSION $GEO_KNOWLEDGE_HUB_ORGANIZATION_URL/$PACKAGE_NAME $PACKAGE_NAME
+        cd $PACKAGE_NAME
+
+        npm install
+        npm run build
+        npm publish --registry $VERDACCIO_LOCAL_REGISTRY
+done
+
+
+#
+# 4. Preparing package to build
+#
+cd $BASE_DIRECTORY
+
+# adding the registry reference in the Dockerfile
+sed -i "/^COPY .\/* /a RUN echo \'@geo-knowledge-hub:registry=${VERDACCIO_DOCKER_REGISTRY}\' > ~/.npmrc" Dockerfile
+
+# excluding temporary files
+FIRST_PACKAGE=`extract_from_quotes ${JAVASCRIPT_DEPENDENCIES_ARRAY[0]}`
+FIRST_PACKAGE=${FIRST_PACKAGE//@geo-knowledge-hub\/''}
+
+rm -rf geo-knowledge-hub ${FIRST_PACKAGE//@geo-knowledge-hub\/''}
